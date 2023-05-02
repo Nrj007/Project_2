5cc22d1d2904af39c51e791f3c7b1791e2a5f919
Nrj007 <niranjn123octopus@gmail.com>
2023-05-02 09:10:40 +0530

    js-file

---
+++ script.js
@@ -0,0 +1,68 @@
+gsap.from(".logo ", 1.2, {
+  opacity: 0,
+  x: 40,
+  ease: "power3.inOut",
+  stagger: 0.2,
+});
+
+gsap.from(".nav-links li", 1.4, {
+  opacity: 0,
+  x: 40,
+  ease: "power3.inOut",
+  stagger: 0.2,
+});
+gsap.from(".hero-container h1 ", 1.4, {
+  delay: 0.5,
+  opacity: 0,
+  x: -100,
+  ease: "power3.inOut",
+  stagger: 0.2,
+});
+gsap.from(".hero-container p ", 1.4, {
+  opacity: 0,
+  x: -200,
+  delay: 0.7,
+  ease: "power2.inOut",
+});
+gsap.from(".box1", 1.3, {
+  opacity: 0.15,
+  x: -200,
+  ease: "circ",
+  scrollTrigger: {
+    trigger: ".box1",
+    scrub: 1,
+    start: "top bottom",
+    end: "center center",
+  },
+});
+gsap.from(".box2", 1.3, {
+  opacity: 0.15,
+  x: 200,
+  ease: "circ",
+  scrollTrigger: {
+    scrub: 1,
+    trigger: ".box2",
+    start: "top bottom",
+    end: "center center",
+  },
+});
+gsap.from(".image_1", 1, {
+  opacity: 1,
+  y: 200,
+  ease: "power2.inOut",
+  scrollTrigger: {
+    scrub: 1.5,
+    trigger: ".image_1",
+    end: "bottom center",
+  },
+});
+gsap.from(".Text_1", 1, {
+  opacity: 0.9,
+  y: 200,
+  ease: "power2.inOut",
+  scrollTrigger: {
+    scrub: 1.5,
+    trigger: ".Text_1",
+    end: "bottom center",
+  },
+});
