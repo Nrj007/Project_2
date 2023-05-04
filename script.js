c339dd561f58b311ecec2031b19da6185c072667
Nrj007 <niranjn123octopus@gmail.com>
2023-05-04 18:08:34 +0530

    animation added

--- script.js
+++ script.js
@@ -75,6 +75,53 @@ gsap.from(".image-wrapper", 1.3, {
   scrollTrigger: {
     scrub: 1.5,
     trigger: ".image-wrapper",
+  },
+});
+gsap.from(".text-wrapper1 p", 1.5, {
+  opacity: -1,
+  y: 400,
+  stagger: 0.4,
+  ease: "power4.inOut",
+  scrollTrigger: {
+    scrub: 1,
+    trigger: ".black",
     end: "center center",
+    duration: 2,
+  },
+});
+gsap.from(".text-wrapper1 h3", 1.5, {
+  opacity: -1,
+  y: 400,
+  stagger: 0.4,
+  ease: "power4.inOut",
+  scrollTrigger: {
+    scrub: 1,
+    trigger: ".black",
+    end: "center center",
+    duration: 2,
+  },
+});
+gsap.from(".text-wrapper1 button", 1.5, {
+  opacity: 0,
+  y: 400,
+  stagger: 0.4,
+  ease: "power4.inOut",
+  scrollTrigger: {
+    scrub: 1,
+    trigger: ".black",
+    end: "center center",
+
+    duration: 2,
+  },
+});
+gsap.from(".image-wrapper1", 1.5, {
+  opacity: 0.7,
+  x: 600,
+  ease: "circ",
+  scrollTrigger: {
+    scrub: 1,
+    trigger: ".black",
+    end: "center center",
+    duration: 2,
   },
 });
